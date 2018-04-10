00310a3cb2b1e3a6a7f71b733bca45656aa1ecdf
Wynforth <jkethethird@gmail.com>
2018-04-10 00:22:25 -0500

    Finish Prior to Personal Review

--- resources/scripts/main.js
+++ resources/scripts/main.js
@@ -1,3 +1,25 @@
 $(document).ready(function(){
   console.log('Ready!');
+  $('.carousel').slick({
+    slidesToScroll: 1,
+    autoplay: true,
+    autoplaySpeed: 4000,
+    arrows: false,
+    draggable: false,
+    pauseOnFocus: false,
+    pauseOnHover: false,
+  })
+
+  let lastUserScroll = 0;
+  $(window).scroll(function(){
+    let scrollTop = $(this).scrollTop();
+    if (scrollTop - lastUserScroll > 50){
+      const navHeight = $('.navbar').css('height');
+      $('.navbar').animate({top: `-${navHeight}`}, 150)
+      lastUserScroll = scrollTop;
+    } else if (lastUserScroll - scrollTop > 50){
+      $('.navbar').animate({top: '0px'}, 150)
+      lastUserScroll = scrollTop;
+    }
+  })
 });
